ace213ee95b11d0de458bd45fafdd23c3ca2540f
jojos38 <jojos38000@hotmail.com>
2020-11-03 20:42:01 +0100

    Fixed error

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ async function checkMessage(lang, message, debug) {
 	if (await db.getSetting(guildID, "warnMessage")) {
 		const warnMessage = await tools.sendCatch(message.channel, lm.getEb(lang).getWarnEmbed(result, debug));
 		await tools.delay(await db.getSetting(guildID, "deleteDelay"));
-		warnMessage.delete();
+		tools.deleteCatch(warnMessage);
 	}
 }
 
